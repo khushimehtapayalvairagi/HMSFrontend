088e26e6cc5bdf75b9042a7b61eeeaa1e25714df
Khushi Mehta <khushimehta82339@gmail.com>
2025-09-01 02:23:05 +0530

    bug fixed

--- src/State/store.js
+++ src/State/store.js
@@ -1,12 +1,12 @@
 // src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import { authReducer, usersReducer } from './Auth/Reducer'; // ✅ fixed
+// import { configureStore } from '@reduxjs/toolkit';
+// import { authReducer, usersReducer } from './Auth/Reducer'; // ✅ fixed
 
 
- const store = configureStore({
-  reducer: {
-    auth: authReducer,
-      users: usersReducer,
-  },
-});
-export default store;
+//  const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//       users: usersReducer,
+//   },
+// });
+// export default store;
