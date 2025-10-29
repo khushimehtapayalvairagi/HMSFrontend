40789b2dcfab28370a4c2e8bb30818e4aaa55831
Khushi Mehta <khushimehta82339@gmail.com>
2025-10-29 20:29:08 +0530

    updated

--- src/Admin/components/AdminDashboard.js
+++ src/Admin/components/AdminDashboard.js
@@ -255,7 +255,7 @@ const AdminDashboard = () => {
           </IconButton>
 
           {/* === Role Menu === */}
-          {/* <Menu anchorEl={roleAnchor} open={Boolean(roleAnchor)} onClose={closeRoleMenu}>
+          <Menu anchorEl={roleAnchor} open={Boolean(roleAnchor)} onClose={closeRoleMenu}>
             <MenuItem
               onClick={(e) => {
                 closeRoleMenu();
@@ -264,16 +264,16 @@ const AdminDashboard = () => {
             >
               👥 Staff ▸
             </MenuItem>
-          </Menu> */}
+          </Menu>
 
           {/* === Staff Submenu === */}
           <Menu anchorEl={staffAnchor} open={Boolean(staffAnchor)} onClose={closeStaffMenu}>
             <MenuItem onClick={(e) => { closeStaffMenu(); openReceptionistMenu(e); }}>
               📋 Receptionist ▸
             </MenuItem>
-            <MenuItem onClick={(e) => { closeStaffMenu(); openNurseMenu(e); }}>
+            {/* <MenuItem onClick={(e) => { closeStaffMenu(); openNurseMenu(e); }}>
               🧑‍⚕️ Nurse ▸
-            </MenuItem>
+            </MenuItem> */}
             <MenuItem onClick={(e) => { closeStaffMenu(); openLabMenu(e); }}>
               🧪 Lab Dashboard ▸
             </MenuItem>
