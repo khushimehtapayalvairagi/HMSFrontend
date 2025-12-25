f3b28d858fc25dd5301eb2cbef83ebef8297aec3
Khushi Mehta <khushimehta82339@gmail.com>
2025-12-25 15:14:08 +0530

    all done

--- src/context/socket.js
+++ src/context/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(process.env.REACT_APP_BASE_URL ,{
-  transports: ["websocket"],
+  // transports: ["websocket"],
   withCredentials: true,
 });
 
